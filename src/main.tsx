4484ccf216a5ae1052abb44d5bee3a2314d2549e
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2022-07-19 22:10:59 -0700

    feat: add poppins font

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import '@fontsource/poppins';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [require('@tailwindcss/line-clamp')],
 };
